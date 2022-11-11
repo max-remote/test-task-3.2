9c1f841be65feae2a73552801b205d877b5346f8
Maxim Meshcheryakov <25-nv-86@mail.ru>
2022-11-11 16:54:41 +0600

    Добавил работу с картинкой во вложенный класс

--- app/src/main/java/com/maks/testtask3_2/ui/MainActivity.java
+++ app/src/main/java/com/maks/testtask3_2/ui/MainActivity.java
@@ -13,11 +13,11 @@ import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final Picture picture = new Picture();
     private final Handler handler = new Handler();
     private ActivityMainBinding binding;
     private String url;
     private ImageView image;
-
     static final long DELAY_TIME = 2000L;
 
     @Override
@@ -26,67 +26,70 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        loadPictureBySearchButton();
-        loadPictureByVirtualEnterKey();
+        picture.loadPictureBySearchButton();
+        picture.loadPictureByVirtualEnterKey();
     }
 
-    /**
-     * загрузка изображения по нажатию иконки поиска в конце строки поиска
-     */
-    private void loadPictureBySearchButton() {
-        binding.inputLayout.setEndIconOnClickListener(v -> loadPicture());
-    }
+    private class Picture {
 
-    /**
-     * загрузка изображения по нажатию клавиши enter на виртуальной клавиатуре
-     */
-    private void loadPictureByVirtualEnterKey() {
-        binding.inputEditText.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                loadPicture();
-                return true;
-            }
-            return false;
-        });
-    }
-
-    /**
-     * Загружает картинку и делает проверку try catch на ввод(null)
-     */
-    private void loadPicture() {
-        String messageWhenNull = this.getString(R.string.toast_message_null);
-        String messageWhenIncorrect = this.getString(R.string.toast_message_incorrect);
-
-        url = binding.inputEditText.getText().toString();
-        image = binding.pictureImageView;
-        try {
-            Picasso.get().load(url).into(image);
-            checkRightLink(messageWhenIncorrect);
-        } catch (Exception e) {
-            doToast(messageWhenNull);
+        /**
+         * загрузка изображения по нажатию иконки поиска в конце строки поиска
+         */
+        private void loadPictureBySearchButton() {
+            binding.inputLayout.setEndIconOnClickListener(v -> loadPicture());
         }
-    }
 
-    /**
-     * Этот метод проверят загрузилась ли картинка в imageView или нет:
-     * если в течении DELAY_TIME загрузка не произошла (по причине неправильной ссылки, опечатки,
-     * неверного ввода и тд), выбросится toastMessage.
-     *
-     * @param toastMessage сообщение, которое выбросится, если картинка не загрузится
-     */
-    private void checkRightLink(String toastMessage) {
-        handler.postDelayed(() -> {
-            if (image.getDrawable() == null) {
-                doToast(toastMessage);
-            } else {
+        /**
+         * загрузка изображения по нажатию клавиши enter на виртуальной клавиатуре
+         */
+        private void loadPictureByVirtualEnterKey() {
+            binding.inputEditText.setOnEditorActionListener((v, actionId, event) -> {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loadPicture();
+                    return true;
+                }
+                return false;
+            });
+        }
+
+        /**
+         * Загружает картинку и делает проверку try catch на ввод(null)
+         */
+        private void loadPicture() {
+            String messageWhenNull = MainActivity.this.getString(R.string.toast_message_null);
+            String messageWhenIncorrect = MainActivity.this.getString(R.string.toast_message_incorrect);
+
+            url = binding.inputEditText.getText().toString();
+            image = binding.pictureImageView;
+            try {
                 Picasso.get().load(url).into(image);
+                checkRightLink(messageWhenIncorrect);
+            } catch (Exception e) {
+                doToast(messageWhenNull);
             }
-        }, DELAY_TIME);
-    }
+        }
 
-    private void doToast(String message) {
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
-        toast.show();
+        /**
+         * Этот метод проверят загрузилась ли картинка в imageView или нет:
+         * если в течении DELAY_TIME загрузка не произошла (по причине неправильной ссылки, опечатки,
+         * неверного ввода и тд), выбросится toastMessage.
+         *
+         * @param toastMessage сообщение, которое выбросится, если картинка не загрузится
+         */
+        private void checkRightLink(String toastMessage) {
+            handler.postDelayed(() -> {
+                if (image.getDrawable() == null) {
+                    doToast(toastMessage);
+                } else {
+                    Picasso.get().load(url).into(image);
+                }
+            }, DELAY_TIME);
+        }
+
+        private void doToast(String message) {
+            Toast toast = Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.show();
+        }
     }
 }
